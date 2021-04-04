0dd9626717d5c7bd56b333c967be64f19a42f27d
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-04-04 15:11:32 +0900

    仮ルートを登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  #devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
+  #get 'sites/index'
+  root to: "sites#index"
+  resources :sites
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
