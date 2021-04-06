c0f3afd95c17e8365c981f39697de4ce8e867cea
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-04-06 18:18:27 +0900

    usersテーブルのkeep_team_idカラムのデフォルト値を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_081147) do
+ActiveRecord::Schema.define(version: 2021_04_06_075931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_081147) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "icon"
-    t.integer "keep_team_id"
+    t.integer "keep_team_id", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
