af44597747be489cf4058347a1748f28019330db
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-04-06 18:21:17 +0900

    usersテーブルのデフォルト値設定を反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-_name = "system"; _email= "system@example.com"; _pass="passwd"; _keep_team_id = nil
-sys_user=User.create!(name: _name, email: _email, password: _pass)
-_name = "test01"; _email= "test01@example.com"; _pass="passwd"; _keep_team_id = nil
-user01=User.create!(name: _name, email: _email, password: _pass)
-_name = "test02"; _email= "test02@example.com"; _pass="passwd"; _keep_team_id = nil
-user02=User.create!(name: _name, email: _email, password: _pass)
-_name = "test03"; _email= "test03@example.com"; _pass="passwd"; _keep_team_id = nil
-user03=User.create!(name: _name, email: _email, password: _pass)
+_name = "system"; _email= "system@example.com"; _pass="passwd"; _keep_team_id = 1
+sys_user=User.create!(name: _name, email: _email, password: _pass, keep_team_id:  _keep_team_id)
+
+_name = "test01"; _email= "test01@example.com"; _pass="passwd"; _keep_team_id = 1
+user01=User.create!(name: _name, email: _email, password: _pass, keep_team_id:  _keep_team_id)
+
+_name = "test02"; _email= "test02@example.com"; _pass="passwd"; _keep_team_id = 1
+user02=User.create!(name: _name, email: _email, password: _pass, keep_team_id:  _keep_team_id)
+
+_name = "test03"; _email= "test03@example.com"; _pass="passwd"; _keep_team_id = 1
+user03=User.create!(name: _name, email: _email, password: _pass, keep_team_id:  _keep_team_id)
 
 _name = "DefaultTeam"
 team00=Team.create!(name: _name, owner_id: sys_user.id)
-_name = "Team01"
-team01=Team.create!(name: _name, owner_id: user01.id)
-_name = "Team02"
-team02=Team.create!(name: _name, owner_id: user02.id)
-
-Assign.create!(user_id: sys_user.id, team_id: team00.id)
-Assign.create!(user_id: user01.id, team_id: team00.id)
-Assign.create!(user_id: user02.id, team_id: team00.id)
