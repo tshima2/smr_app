39d5b1cae13dad83824b8bca5374788438fabf62
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-04-20 21:31:03 +0900

    バリデーション追加

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,11 +1,12 @@
 class Site < ApplicationRecord
   acts_as_taggable
-  
-  validates :name, presence: true
-  validates :address, presence: true  
+
+  validates :name, presence: true, length: {maximum: 128}
+  validates :address, presence: true, length: {maximum: 255}
+  validates :memo, length: {maximum: 255}
 
   has_many :comments, dependent: :destroy
-  has_many :image_posts, dependent: :destroy  
+  has_many :image_posts, dependent: :destroy
   belongs_to :user
   belongs_to :team
 
